d714d1cc7113f7bcdd9161d7910bd3d51aef8430
Ezequiel Garibotti <garibottieze@gmail.com>
2025-01-22 19:19:28 -0300

    Use service to send emails

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens([
+            'resend/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (AuthenticationException $exception) {
